1c4ad354460956e915545cdf810c0ef29d44c5b4
Duo <38347769+tiandi1994@users.noreply.github.com>
2018-04-23 09:39:07 +0100

    Update AbsAmp_interp.m

--- AbsAmp_interp.m
+++ AbsAmp_interp.m
@@ -1,33 +1,33 @@
-mask = 20*log10(ImageBS)>50;
-AbsImAmpVM2 = AbsImAmpVM1.*mask;
+mask = ImageBS>120;
+AbsImAmpVM2 = AbsImAmpVM1.*mask.*[zeros(20,512);ones(430,512);zeros(512-450,512)];
 pixel = 1:512;
 interpRe = zeros(512,512);
 for i = 1:512
-    zeroEle = find(AbsImAmpVM2(:,i)==0);
     noneZeroEle = find(AbsImAmpVM2(:,i));
+    if length(noneZeroEle)<=2
+        continue
+    else
     interpRe(:,i) = interp1(noneZeroEle,AbsImAmpVM2(noneZeroEle,i),1:512,'linear',0);
+    end
 end
 figure;imagesc(AbsImAmpVM2,[0 70]);colormap(jet);colorbar
 figure;imagesc(interpRe,[0 70]);colormap(jet);colorbar
 strain = zeros(510,512);
+m=4;
 for j=1:512
     for i=1:512-m+1
-        strain(i,j)=(6*(2*(i-i)-m+1))*interpRe(i,j)/(m^3-m)*i+(6*(2*(i+1-i)-m+1))...
-            *interpRe(i+1,j)/(m^3-m)*(i+1)+(6*(2*(i+2-i)-m+1))*interpRe(i+2,j)/(m^3-m)*(i+2);...
-            %+(6*(2*(i+3-i)-m+1))*AbsImAmpVM1(i+3,j)/(m^3-m)*(i+3)+(6*(2*(i+4-i)-m+1))*AbsImAmpVM1(i+4,j)/(m^3-m)*(i+4);
+        strain(i,j)=(6*(2*(i-i)-m+1))*interpRe(i,j)/(m^3-m)+(6*(2*(i+1-i)-m+1))...
+            *interpRe(i+1,j)/(m^3-m)+(6*(2*(i+2-i)-m+1))*interpRe(i+2,j)/(m^3-m)...
+            +(6*(2*(i+3-i)-m+1))*interpRe(i+3,j)/(m^3-m);%+(6*(2*(i+4-i)-m+1))*AbsImAmpVM1(i+4,j)/(m^3-m)*(i+4);
     end
 end
-Youngs=1./abs(strain);
+Youngs=1./abs(strain+1e-7);
+Youngs = Youngs.*mask(1:510,:).*[zeros(20,512);ones(430,512);zeros(510-450,512)];
 
-Y3=193*50*medfilt2(Youngs,[2 2]);
 
-% mask = (20*log10(abs(squeeze(ComplexFrames(:,:,100))))>40).*(Snr>50);
-% gra = gradient(AbsImAmpVM1,5);
-% figure;imagesc(medfilt2(abs(1./gra)*80,[3 3]),[0 300]);colormap(jet);title('gradient')
-figure;imagesc(Y3,[0 300]);colormap(jet);title('after strain stry CompAfter');colorbar
-% figure;imagesc(medfilt2(AbsImAmpVM1,[1 1]),[0 3000]);colormap(jet);title('Vibration Amp CompAfter')
-% Elas=interp1(K(1:510),Y3,KES(1:510),'linear','extrap'); %Dispersion Compensation
-% a = mean(abs(ComplexFrames(1:510,:,:)),3);
-% b = medfilt2(1e4*Youngs./a,[2 2]);
-% figure;imagesc(1e4*b,[0 80]);colormap(jet);title('after comp');
-toc
+Y3=Youngs;
+
+
+
+figure;imagesc(medfilt2(Y3.*[zeros(50,512);ones(300,512);zeros(510-350,512)],[5 5]),[0 1500]);colormap(jet);title('after strain stry CompAfter');colorbar
+
